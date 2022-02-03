3e0b7a8cbd5daab6a6f45a0e82a4a249dd802846
Sonicbasedrop <ivan.ulysses@hotmail.com>
2022-02-03 21:04:09 +0000

    Added forms, admin and signals

--- checkout/signals.py
+++ checkout/signals.py
@@ -9,7 +9,7 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
     """
-    instance.order.upadet_total()
+    instance.order.update_total()
 
 
 @receiver(post_delete, sender=OrderLineItem)
