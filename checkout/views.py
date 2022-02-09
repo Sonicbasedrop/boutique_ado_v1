dfe5869402d94bbbf614ff7ccd46581082648adf
Sonicbasedrop <ivan.ulysses@hotmail.com>
2022-02-09 21:24:54 +0000

    Added srtipe public key

--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe__public_key': 'pk_test_51KRELIIkA8ERQ8dp2tdsHrSePo1ZJLxi6kettukRoMINh3Dzanzi1abdumEARHWSkERCaVWA0Vg1eICoctjqwR2T00Jdnsdvqq',
+        'stripe__public_key': 'sk_test_51KRELIIkA8ERQ8dp8Snubxw0mJA5Ofol2IPNvIx4udRqv6NoJgAy07at7siBj5Fj8InN3uMscxcoHZszoDCuPvMp00yVIeYLuw',
         'client_secret': 'test client secret',
     }
 
