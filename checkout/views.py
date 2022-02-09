8370ac60442c1030b10aa86c9d9d1991ac5ad59f
Sonicbasedrop <ivan.ulysses@hotmail.com>
2022-02-09 14:24:21 +0000

    Fixed some spelling errors etc

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe__public_key': 'pk_test_51KRELIIkA8ERQ8dp2tdsHrSePo1ZJLxi6kettukRoMINh3Dzanzi1abdumEARHWSkERCaVWA0Vg1eICoctjqwR2T00Jdnsdvqq',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
